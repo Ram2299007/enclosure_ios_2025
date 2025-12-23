3f2be7c1968f6a79ff45931b38236153feaf1c4a
ramlohar <ramlohar03.rsl@gmail.com>
2025-12-23 15:19:59 +0530

    Remove inner fill from sender voice audio bubble

--- Enclosure/Screens/ChattingScreen.swift
+++ Enclosure/Screens/ChattingScreen.swift
@@ -6521,7 +6521,7 @@ struct SenderDocumentView: View {
                     .frame(maxHeight: .infinity, alignment: .center) // Vertically center with play button
                 }
                 .padding(.horizontal, 3) // Android: layout_marginHorizontal="3dp"
-                .background(backgroundColor) // Match outer bubble color in both light/dark
+                .background(Color.clear) // Remove inner fill to avoid darker rectangle
             }
             .padding(.horizontal, 7) // Android: layout_marginHorizontal="7dp"
             .padding(.vertical, 7) // Android: layout_marginVertical="7dp"
