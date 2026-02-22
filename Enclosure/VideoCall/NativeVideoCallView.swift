313865165e5fd443676f5e030d9c88c2b6b93205
ramlohar <ramlohar03.rsl@gmail.com>
2026-02-22 11:53:59 +0530

    Remove duplicate Color(hex:) extension - already exists in callView.swift

--- Enclosure/VideoCall/NativeVideoCallView.swift
+++ Enclosure/VideoCall/NativeVideoCallView.swift
@@ -11,25 +11,6 @@ import SwiftUI
 import AVFoundation
 import WebRTC
 
-// MARK: - Helper: Color from hex string
-private extension Color {
-    init(hex: String) {
-        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
-        var int: UInt64 = 0
-        Scanner(string: hex).scanHexInt64(&int)
-        let r, g, b: Double
-        switch hex.count {
-        case 6:
-            r = Double((int >> 16) & 0xFF) / 255
-            g = Double((int >> 8) & 0xFF) / 255
-            b = Double(int & 0xFF) / 255
-        default:
-            r = 0; g = 0.64; b = 0.91 // fallback #00A3E9
-        }
-        self.init(red: r, green: g, blue: b)
-    }
-}
-
 // MARK: - NativeVideoCallView
 
 struct NativeVideoCallView: View {
